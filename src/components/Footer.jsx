878600952d5b32bb49805ff658cabbd0393e7756
Zaheer khan <newzk16117541@gmail.com>
2023-04-12 18:29:07 +0530

    Added footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
+import { Link } from 'react-router-dom';
+
 //Footer
 const Footer = () => {
   return (
-    <div className="p-6 bg-amber-300 text-blue-600 text-lg  bottom-0 left-0 w-full">
-      <h4>Footer</h4>
+    <div className="p-6 bg-amber-300 text-blue-600 text-lg  bottom-0 left-0 w-full flex justify-center">
+      <h3>Made with ❤️ by </h3>
+      <h2 className="hover:text-blue-900 font-bold px-1">
+        {' '}
+        {<Link to="http://zaheer-zk.github.io/Advanced-porfolio/">Zaheer</Link>}
+      </h2>
     </div>
   );
 };
