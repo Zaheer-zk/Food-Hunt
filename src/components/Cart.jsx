5efc1f4a8c83c0559f3a9100841100ab276d7bf5
Zaheer khan <newzk16117541@gmail.com>
2023-04-15 11:33:27 +0530

    Added clear cart code

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import store from '../utils/store';
 import CartCard from './CartCard';
+import { clearCart } from '../utils/cartSlice';
 
 const Cart = () => {
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
   const cartItems = useSelector((store) => store.cart.items);
   return (
     <div className="flex bg-blue-900 text-white items-center flex-wrap">
       <h1>Cart Items - {cartItems.length}</h1>
+      <button onClick={() => handleClearCart()} className="bg-red-300 p-2 m-2">
+        Clear cart
+      </button>
       {cartItems.map((cartItem, i) => (
         <CartCard name={cartItem} key={i} index={i + 1} />
       ))}
