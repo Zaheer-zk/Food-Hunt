baf4ec40b651b8c3dd3fc6438d7a77a1f9853154
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2023-11-18 13:51:25 +0530

    feat: Added error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -4,16 +4,16 @@ import { useRouteError } from 'react-router-dom';
 const Error = () => {
   const err = useRouteError();
   return (
-    <div className='min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-red-500 to-yellow-500'>
-      <div className='text-center text-white'>
+    <div className='min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-gray-800 to-gray-900 text-white'>
+      <div className='text-center'>
         <h2 className='text-6xl font-extrabold mb-4'>Oops!!</h2>
         <p className='text-2xl mb-2'>Something went wrong...</p>
         <p className='text-md'>
-          Getting statusCode {err.status} : {err.statusText}
+          Getting status code {err.status} : {err.statusText}
         </p>
         <a
           href='/'
-          className='inline-block mt-4 bg-white text-red-500 px-6 py-3 rounded-md shadow hover:bg-gray-100 transition duration-300'
+          className='inline-block mt-4 bg-white text-gray-800 px-6 py-3 rounded-md shadow hover:bg-gray-100 transition duration-300'
         >
           Go Back Home
         </a>
