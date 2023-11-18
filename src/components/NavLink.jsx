eff3b82f8c1cf979f980294b58919e58891e80f9
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2023-11-18 17:09:06 +0530

    Added UI Fix- Mobile navbar

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -5,6 +5,14 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 // import { useUser } from '@clerk/clerk-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '../../components/ui/sheet.jsx';
 
 const NavLink = () => {
   // const { user } = useContext(UserContext);
@@ -21,7 +29,7 @@ const NavLink = () => {
 
   return (
     <nav className='text-lg flex justify-between items-center'>
-      <ul className='flex flex-wrap justify-center gap-4 md:gap-8'>
+      <ul className='hidden lg:flex flex-wrap justify-center gap-4 md:gap-8'>
         <li>
           <Link
             to='/'
@@ -63,6 +71,59 @@ const NavLink = () => {
         {/* )} */}
       </ul>
       <UserButton />
+      <Sheet>
+        <SheetTrigger className='lg:hidden pl-2'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            fill='currentColor'
+            viewBox='0 0 24 24'
+            className='hamburger-icon'
+          >
+            <path d='M3,12H21a1,1,0,0,0,0-2H3a1,1,0,0,0,0,2Z' />
+            <path d='M3,6H21a1,1,0,0,0,0-2H3A1,1,0,0,0,3,6Z' />
+            <path d='M21,16H3a1,1,0,0,0,0,2H21a1,1,0,0,0,0-2Z' />
+          </svg>
+        </SheetTrigger>
+        <SheetContent>
+          <ul className='flex flex-wrap flex-col justify-center gap-4 md:gap-8 text-teal-50'>
+            <li>
+              <Link
+                to='/'
+                className='hover:text-indigo-500 transition-colors duration-300'
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/about'
+                className='hover:text-indigo-500 transition-colors duration-300'
+              >
+                About us
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/cart'
+                className='hover:text-indigo-500 transition-colors duration-300'
+              >
+                Cart ({cartItemsCount})
+              </Link>
+            </li>
+            <li>
+              <SignedOut>
+                <Link to='/sign-up'>
+                  <button className='bg-blue-400 hover:bg-blue-500 text-white py-2 px-3 rounded transition-colors duration-300'>
+                    Login
+                  </button>
+                </Link>
+              </SignedOut>
+            </li>
+          </ul>
+        </SheetContent>
+      </Sheet>
     </nav>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,jsx}'],
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+	],
   theme: {
-    fontFamily: {
-      body: ['Space Grotesk', 'sans-serif'],
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [require('@tailwindcss/forms')],
-};
+  plugins: [require("tailwindcss-animate")],
+}
